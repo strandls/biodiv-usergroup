82955bf8b05b3af01b12301871ff8792087c49dd
Abhishek Kumar Rudra <abhishek@darbhanga.strandls.com>
2019-08-22 15:48:23 +0530

    Swagger Plugin

--- src/main/java/com/strandls/userGroup/controller/UserGroupController.java
+++ src/main/java/com/strandls/userGroup/controller/UserGroupController.java
@@ -24,8 +24,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.SwaggerDefinition;
-import io.swagger.annotations.Tag;
 
 /**
  * @author Abhishek Rudra
@@ -33,8 +31,6 @@ import io.swagger.annotations.Tag;
  */
 
 @Api("UserGroup Serivce")
-@SwaggerDefinition(tags = {
-		@Tag(name = "User Group Serivce to get Group Details", description = "Rest endpoint for UserGroup Service") })
 @Path(ApiConstants.V1 + ApiConstants.GROUP)
 public class UserGroupController {
 
@@ -74,16 +70,17 @@ public class UserGroupController {
 			return Response.status(Status.BAD_REQUEST).build();
 		}
 	}
-	
+
 	@GET
-	@Path(ApiConstants.OBSERVATION+"/{observationId}")
+	@Path(ApiConstants.OBSERVATION + "/{observationId}")
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
-	
+
 	@ApiOperation(value = "Find UserGroup by observation ID", notes = "Returns UserGroup Details", response = UserGroupIbp.class)
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = UserGroupIbp.class,responseContainer = "List"),
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Success", response = UserGroupIbp.class, responseContainer = "List"),
 			@ApiResponse(code = 404, message = "Traits not found", response = String.class) })
-	
+
 	public Response getObservationUserGroup(@PathParam("observationId") String observationId) {
 		try {
 			Long id = Long.parseLong(observationId);
@@ -92,7 +89,7 @@ public class UserGroupController {
 		} catch (Exception e) {
 			return Response.status(Status.BAD_REQUEST).build();
 		}
-		
+
 	}
 
 }
