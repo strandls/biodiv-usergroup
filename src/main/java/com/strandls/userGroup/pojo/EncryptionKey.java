9b03d2c50d6807957fdfd14889ef32fc72ee89a3
Abhishek Kumar Rudra <abhishek.rudra02@gmail.com>
2020-06-19 00:01:30 +0530

    method type changed

---
+++ src/main/java/com/strandls/userGroup/pojo/EncryptionKey.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.strandls.userGroup.pojo;
+
+/**
+ * @author Abhishek Rudra
+ *
+ */
+public class EncryptionKey {
+
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
