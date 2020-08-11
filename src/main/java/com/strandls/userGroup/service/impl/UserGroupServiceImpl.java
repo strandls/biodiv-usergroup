82aa96f51f917189f87b4f57d3e65a4fe61a4d6e
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-08-11 11:34:30 +0530

    :recycle: log

--- src/main/java/com/strandls/userGroup/service/impl/UserGroupServiceImpl.java
+++ src/main/java/com/strandls/userGroup/service/impl/UserGroupServiceImpl.java
@@ -1430,6 +1430,7 @@ public class UserGroupServiceImpl implements UserGroupSerivce {
 				}
 			}
 		} catch (Exception ex) {
+			ex.printStackTrace();
 			logger.error(ex.getMessage());
 		}
 		return userData;
